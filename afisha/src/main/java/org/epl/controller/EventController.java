9a7cacee4696807e39e0691e68d4ffcf5e8185fe
Leonid Kornev <lenich.ru@mail.ru>
2016-11-29 20:50:23 +0300

    Merge remote-tracking branch 'remotes/origin/Leonid' into HEAD
    # Conflicts:
    #	afisha/src/main/java/org/epl/controller/EventController.java
    #	afisha/src/main/java/org/epl/model/Type.java

--- afisha/src/main/java/org/epl/controller/EventController.java
+++ afisha/src/main/java/org/epl/controller/EventController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,13 @@ public class EventController {
     private ImageService imageService;
     @Autowired
     private UserService userService;
+
+    @RequestMapping(method=RequestMethod.GET, value="/event/{id}")
+    public String getEvent(@PathVariable int id, ModelMap model) {
+        model.addAttribute("types", typeService.findAllType());
+        model.addAttribute("event", eventService.findById(id));
+        return "event";
+    }
 
     @RequestMapping(value={"/new"}, method = RequestMethod.GET)
     public String newEvent(ModelMap model)
